b0101cecfc2b2f42b54404934b54d9b907d79d67
Johan De Jesus <74220153+johandpr@users.noreply.github.com>
2022-02-06 22:48:23 -0400

    add channel message event

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-var config = {
-	channels: ["#puertorico"],
-	server: "irc.rizon.net",
-	name: "prbot" + getRandomInt(1000).toString()
-};
-
+var settings = require('./settings.json')
 var irc = require("irc");
 
-// Create the bot name
-var bot = new irc.Client(config.server, config.name, {
-	channels: config.channels
+var bot = new irc.Client(settings.server, settings.nickname, {
+	channels: settings.channels
 });
-
-
 
 bot.addListener('error', function(message) {
     console.log('error: ', message);
+});
+
+bot.addListener('message#', function (nick, to, text, message) {
+    console.log('-> [',message.nick,'] ',message.args[1]);
 });
